7752d3447b43db71b1b19d3d3567f40747421af2
shane <shane@argos-soultions.io>
2020-05-09 21:49:15 +0900

    first commit

--- config/settings.py
+++ config/settings.py
@@ -79,18 +79,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'databasecenter',
-        'USER': 'shane',
-        'PASSWORD': 'eodhkd12',
-        'HOST': 'databasecenter.cadwt7paurr9.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-}
 
 
 # Password validation
